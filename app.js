d70ce07696263984042cb8ef08e8f962d8e26597
Pocket <p0ck31@gmail.com>
2015-01-07 14:53:33 -0800

    Hello world angular

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('flapperNews', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.test = 'Hello world!';
+}]);
+
+
+// angular.module('flapperNews', [])
+// .controller('MainCtrl', [
+// '$scope',
+// function($scope){
+//   $scope.test = 'Hello world!';
+// }]);
