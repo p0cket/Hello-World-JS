73889fb4703916bdc9d5f98479813a3d82e76872
Pocket <p0ck31@gmail.com>
2015-01-08 11:51:12 -0800

    add home template & notice things broke .factory
    Things broke before at .factory

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+  'stateProvider',
+  'urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+}])
+
+
 .controller('MainCtrl', [
 '$scope',
 'posts',
