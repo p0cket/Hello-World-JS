32739546469ea21002c337de107bf6c29a32e258
Pocket <p0ck31@gmail.com>
2015-01-08 16:37:12 -0800

    About to leave AWS commit
    <span>
      <a href="#/posts/{{$index}}">Comments</a>
    </span>

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ angular.module('flapperNews', ['ui.router'])
       templateUrl: '/home.html',
       controller: 'MainCtrl'
     });
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts/html',
+      controller: 'PostsCtrl'
+    });
 
     $urlRouterProvider.otherwise('home');
 }])
 
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+functions($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+
+}]);
 
 .controller('MainCtrl', [
 '$scope',
@@ -28,6 +41,8 @@ function($scope,posts){
   //   {title: 'post 5', upvotes: 4}
   //   ];
 
+
+
   $scope.posts = posts.posts;
 
   $scope.addPost = function(){
@@ -37,10 +52,15 @@ function($scope,posts){
       title: $scope.title,
       link: $scope.link,
       upvotes: 0
+      comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
     console.log("refresh");
+
   };
 
   $scope.incrementUpvotes = function(post) {
